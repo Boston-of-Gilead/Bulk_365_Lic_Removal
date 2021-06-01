0dde1ad15e2a66bab2d0c50366b4b0379314e902
Boston-of-Gilead <Boston-of-Gilead@users.noreply.github.com>
2021-06-01 07:55:53 -0400

    Add files via upload

---
+++ Bulk_Delete.ps1
@@ -0,0 +1,13 @@
+﻿#Bulk user license removal in 365
+
+#Connect-MsolService
+
+#Get-content -path $list
+$list = Get-Content -Path "C:\path_to\bulkdelete.txt"
+
+forEach ($i in $list){
+    #get-MsolUser -UserPrincipalName $i.licenses.AccountSkuId | 
+    Set-MsolUserLicense -UserPrincipalName $i -RemoveLicenses "<>:STANDARDPACK_<>"
+    Remove-MsolUser -UserPrincipalName $i -Force
+    Write-host "Unlicensed and removed " + $i
+    } 
